f1a63af6a1fe19d920ce8a985eb0a85c37e0c478
Leonardo Bernardo <leonardo.bernardo2658@gmail.com>
2025-08-22 19:36:05 -0300

    feat: definição de rota para imoveis

--- src/main.tsx
+++ src/main.tsx
@@ -6,6 +6,7 @@ import theme from "./theme/theme.ts";
 import { BrowserRouter, Route, Routes } from "react-router";
 import Services from "./pages/Services.tsx";
 import Home from "./pages/Home.tsx";
+import Properties from "./pages/Properties.tsx";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
@@ -16,6 +17,7 @@ createRoot(document.getElementById("root")!).render(
           <Route path="/" element={<App />}>
             <Route index element={<Home />} />
             <Route path="servicos" element={<Services />} />
+            <Route path="imoveis" element={<Properties />} />
           </Route>
         </Routes>
       </BrowserRouter>
---
+++ src/pages/Properties.tsx
@@ -0,0 +1,13 @@
+import { Box } from "@mui/material";
+import type { ReactElement } from "react";
+
+export default function Properties(): ReactElement {
+  return (
+    <Box
+      sx={{
+        width: "100vw",
+        height: "100vh",
+      }}
+    ></Box>
+  );
+}
