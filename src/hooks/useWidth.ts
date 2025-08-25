b93549b71cb2395a8b0bf1261651ef2b8c9fb5bf
Leonardo Bernardo <leonardo.bernardo2658@gmail.com>
2025-08-25 23:04:07 +0000

    fix: trocando hook personalizado por useMediaQuery

--- src/hooks/useWidth.ts
+++ src/hooks/useWidth.ts
@@ -1,22 +1,5 @@
-import { useEffect, useState } from "react";
+import { useMediaQuery, type Breakpoint } from "@mui/material";
 
-export default function useWidth(): number {
-  // Definição das variáveis reativas
-  const [width, setWidth] = useState<number>(window.innerWidth);
-
-  // Função que ao ser disparada, armazena o novo valor da largura da tela
-  const handleResize = () => setWidth(window.innerWidth);
-
-  useEffect(() => {
-    // Adiciona um evento que dispara a função sempre que um efeito de
-    // alteração da largura é disparado
-    window.addEventListener("resize", handleResize);
-
-    // Remove o listener
-    return () => {
-      window.removeEventListener("resize", handleResize);
-    };
-  }, []); // monta/desmonta apenas uma vez
-
-  return width;
+export default function useWidth(mediaQuery: number | Breakpoint): boolean {
+  return useMediaQuery((theme) => theme.breakpoints.up(mediaQuery))
 }
