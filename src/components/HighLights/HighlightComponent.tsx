dfa71065fab9fb9b7b91d55a64b53504e8becb11
Leonardo Bernardo <leonardo.bernardo2658@gmail.com>
2025-09-01 20:01:05 -0300

    modified:   src/components/HighLights/HighlightComponent.tsx

--- src/components/HighLights/HighlightComponent.tsx
+++ src/components/HighLights/HighlightComponent.tsx
@@ -1,4 +1,4 @@
-import { Box, Typography } from "@mui/material";
+import { Typography } from "@mui/material";
 import type { ReactElement } from "react";
 import ImageComponent from "../ImageComponent";
 import PaperImage from "../ui/PaperImage";
