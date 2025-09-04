5d856ad4e4d785511dc79f243c1619c0a6dd9443
Leonardo Bernardo <leonardo.bernardo2658@gmail.com>
2025-09-03 22:40:53 -0300

    new: novo componente para manipular a descricao do highlight

---
+++ src/components/HighLights/DescriptionHighlight.tsx
@@ -0,0 +1,14 @@
+import { Typography } from "@mui/material";
+import type { ReactElement } from "react";
+
+export default function DescriptionHighlight(props: { description: string }): ReactElement {
+
+    const limitString = (limit: number): string => {
+        if (props.description.length > limit) return `${props.description.slice(0, limit)}...`;
+        return props.description;
+    }
+
+    return (
+        <Typography color="textSecondary">{limitString(100)}</Typography>
+    )
+}
