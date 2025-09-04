7a4b5d1cfb944cb2a620e07d71fd499b7ee9dc4b
Leonardo Bernardo <leonardo.bernardo2658@gmail.com>
2025-09-04 19:16:43 -0300

    new: novo componente para exibição de label

---
+++ src/components/HighLights/LabelHighlight.tsx
@@ -0,0 +1,22 @@
+import { Paper, Typography } from "@mui/material";
+import type { ReactElement } from "react";
+
+export default function LabelHighlight(props: { labelName: string }): ReactElement {
+    return (
+        <Paper variant="outlined" sx={{
+            position: 'absolute',
+            top: 15,
+            right: 15,
+            width: 'fit-content',
+            color: (theme) => theme.palette.primary.dark,
+            paddingX: (theme) => theme.spacing(2),
+            borderRadius: (theme) => theme.shape.borderRadius,
+        }}>
+            <Typography sx={{
+                fontSize: '.8rem', fontWeight: '400',
+            }}>
+                {props.labelName}
+            </Typography>
+        </Paper>
+    )
+}
