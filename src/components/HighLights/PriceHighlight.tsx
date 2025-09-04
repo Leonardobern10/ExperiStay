8daa168b50d455e58dc55e5da5e841b5706d79c1
Leonardo Bernardo <leonardo.bernardo2658@gmail.com>
2025-09-03 22:41:08 -0300

    new: componente que renderiza o preco estilizado

---
+++ src/components/HighLights/PriceHighlight.tsx
@@ -0,0 +1,13 @@
+import { Typography } from "@mui/material";
+import type { ReactElement } from "react";
+
+export default function PriceHighlight(props: {
+    value: number
+}): ReactElement {
+    return (
+        <Typography variant="h3">
+            R$ {props.value}
+            <Typography component='span'>/noite</Typography>
+        </Typography>
+    )
+}
