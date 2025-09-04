c17b9d4efb68e1658a8319b0536dc0a1918c4094
Leonardo Bernardo <leonardo.bernardo2658@gmail.com>
2025-09-04 19:16:14 -0300

    fix: alterando estiliazação

--- src/components/RatingValue.tsx
+++ src/components/RatingValue.tsx
@@ -4,9 +4,11 @@ import StarIcon from '@mui/icons-material/Star';
 
 export default function RatingValue(props: { value: number }): ReactElement {
     return (
-        <Box component='div' sx={{ width: 'fit-content', height: '2rem', padding: (theme) => theme.spacing(2), position: 'absolute', top: '85%', left: '5%', backgroundColor: (theme) => theme.palette.background.default, display: 'flex', flexDirection: 'row', justifyContent: 'space-between', alignItems: 'center', columnGap: (theme) => theme.spacing(2), borderRadius: (theme) => theme.shape.borderRadius }}>
-            <StarIcon color='primary' />
-            <Typography variant="h6">{props.value.toPrecision(2)}</Typography>
+        <Box component='div' sx={{ width: '5rem', padding: (theme) => theme.spacing(1), position: 'absolute', top: '85%', left: '5%', backgroundColor: (theme) => theme.palette.background.default, display: 'flex', flexDirection: 'row', justifyContent: 'space-evenly', alignItems: 'center', columnGap: (theme) => theme.spacing(1), borderRadius: (theme) => theme.shape.borderRadius }}>
+            <StarIcon sx={{
+                height: '1.2rem'
+            }} color='primary' />
+            <Typography variant="body1">{props.value.toPrecision(2)}</Typography>
         </Box>
     )
 }
