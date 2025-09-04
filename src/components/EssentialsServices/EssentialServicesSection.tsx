a655ecca4a8d5bbe492d50ca938a1f0e4f1ea5e5
Leonardo Bernardo <leonardo.bernardo2658@gmail.com>
2025-09-03 22:40:08 -0300

    fix: corrigindo importação

--- src/components/EssentialsServices/EssentialServicesSection.tsx
+++ src/components/EssentialsServices/EssentialServicesSection.tsx
@@ -2,7 +2,7 @@ import type { ReactElement } from "react";
 
 export default function EssentialServicesSection(props: { className: string }): ReactElement {
     return (
-        <div className={props.className} style={{ width: '100%', height: '100vw', border: 2, borderColor: 'red' }}>
+        <div className={props.className} style={{ width: '100%', height: '100vw' }}>
             a asdasd
         </div>
     )
