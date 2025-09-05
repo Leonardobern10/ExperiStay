5e96ceacaaf2679942b973b0cca2f5861f9e5b8b
Leonardo Bernardo <leonardo.bernardo2658@gmail.com>
2025-09-05 18:30:12 -0300

    add: inserção de imagens e atualização dos dados

--- src/data/exclusiveDestinyData.ts
+++ src/data/exclusiveDestinyData.ts
@@ -1,9 +1,12 @@
 import type { ExclusiveDestinyProps } from '../types/ExcluviseDestinyProps';
-import imgTest from '../assets/images/highlights/florianopolis_highlight.png';
+import lencoisMaranhenses from '../assets/images/highlights/lencois_maranhenses.png';
+import pelourinhoBahia from '../assets/images/highlights/pelourinho_BA.png';
+import jericoacoara from '../assets/images/highlights/jericoacoara_RE.png';
+import rioAmazonas from '../assets/images/highlights/rio_amazonas.png';
 
 export const exclusiveDestines: Array<ExclusiveDestinyProps> = [
-     { index: 0, name: 'Lençóis Maranhenses', available: 5, img: imgTest },
-     { index: 1, name: 'Lençóis Maranhenses', available: 5, img: imgTest },
-     { index: 2, name: 'Lençóis Maranhenses', available: 5, img: imgTest },
-     { index: 3, name: 'Lençóis Maranhenses', available: 5, img: imgTest },
+     { index: 0, name: 'Lençóis Maranhenses', available: 5, img: lencoisMaranhenses },
+     { index: 1, name: 'Pelourinho', available: 3, img: pelourinhoBahia },
+     { index: 2, name: 'Jericoacoara', available: 4, img: jericoacoara },
+     { index: 3, name: 'Rio Amazonas', available: 2, img: rioAmazonas },
 ];
