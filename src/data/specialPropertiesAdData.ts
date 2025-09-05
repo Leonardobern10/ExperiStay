4aa153727a6f6802f2d572c7114c82d72d4cd8ff
Leonardo Bernardo <leonardo.bernardo2658@gmail.com>
2025-09-05 20:22:00 -0300

    new: dados para preencher as vantages

---
+++ src/data/specialPropertiesAdData.ts
@@ -0,0 +1,27 @@
+import type { AdvantageAdProps } from '../types/AdvantagesAdProps';
+
+export const dataContainer = {
+     title: 'Tem um imóvel especial',
+     subtitle:
+          'Junte-se à nossa comunidade de anfitriões e transforme seu imóvel único em uma fonte de experiências inesquecíveis para viajantes de todo o Brasil',
+     buttonRegister: 'Cadastrar meu imóvel',
+     buttonMore: 'Saiba mais',
+};
+
+export const advantages: Array<AdvantageAdProps> = [
+     {
+          index: 0,
+          title: 'Renda extra',
+          description: 'Monetize seu imóvel com nossa plataforma premmium',
+     },
+     {
+          index: 1,
+          title: 'Renda extra',
+          description: 'Monetize seu imóvel com nossa plataforma premmium',
+     },
+     {
+          index: 2,
+          title: 'Renda extra',
+          description: 'Monetize seu imóvel com nossa plataforma premmium',
+     },
+];
