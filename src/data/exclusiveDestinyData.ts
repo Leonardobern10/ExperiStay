cccdcccaa2cd7cff66ff512495f664bba0bde10b
Leonardo Bernardo <leonardo.bernardo2658@gmail.com>
2025-09-05 16:40:25 -0300

    new: novos dados para o container exclusiveDestines

---
+++ src/data/exclusiveDestinyData.ts
@@ -0,0 +1,9 @@
+import type { ExclusiveDestinyProps } from '../types/ExcluviseDestinyProps';
+import imgTest from '../assets/images/highlights/florianopolis_highlight.png';
+
+export const exclusiveDestines: Array<ExclusiveDestinyProps> = [
+     { index: 0, name: 'Lençóis Maranhenses', available: 5, img: imgTest },
+     { index: 1, name: 'Lençóis Maranhenses', available: 5, img: imgTest },
+     { index: 2, name: 'Lençóis Maranhenses', available: 5, img: imgTest },
+     { index: 3, name: 'Lençóis Maranhenses', available: 5, img: imgTest },
+];
