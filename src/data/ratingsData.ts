c50f59aef427067b92809089e583718a261bb3e3
Leonardo Bernardo <leonardo.bernardo2658@gmail.com>
2025-09-05 19:47:25 -0300

    new: dados das avaliações dos usuários

---
+++ src/data/ratingsData.ts
@@ -0,0 +1,28 @@
+import type { RatingProps } from '../types/RatingProps';
+
+export const ratings: Array<RatingProps> = [
+     {
+          index: 0,
+          rating: 5,
+          textRating:
+               'A experiência foi simplesmente mágica! Nadar com golfinhos em Fernendo de Noronha foi um sonho realizado. A vila era perfeita e a equipe incrível.',
+          name: 'Maria Clara',
+          location: 'São Paulo, SP',
+     },
+     {
+          index: 1,
+          rating: 4,
+          textRating:
+               'A experiência foi simplesmente mágica! Nadar com golfinhos em Fernendo de Noronha foi um sonho realizado. A vila era perfeita e a equipe incrível.',
+          name: 'José Inácio',
+          location: 'São Paulo, SP',
+     },
+     {
+          index: 2,
+          rating: 5,
+          textRating:
+               'A experiência foi simplesmente mágica! Nadar com golfinhos em Fernendo de Noronha foi um sonho realizado. A vila era perfeita e a equipe incrível.',
+          name: 'Maria Clara',
+          location: 'São Paulo, SP',
+     },
+];
