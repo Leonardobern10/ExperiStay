2e373fa80764f5a988e85789c2e201725dc85485
Leonardo Bernardo <leonardo.bernardo2658@gmail.com>
2025-09-05 16:40:08 -0300

    fix: inserindo interativadade

--- src/components/ExclusiveDestinys/ExclusiveDestinyContainer.tsx
+++ src/components/ExclusiveDestinys/ExclusiveDestinyContainer.tsx
@@ -1,6 +1,8 @@
 import { Box, Stack, Typography } from '@mui/material';
 import type { ReactElement } from 'react';
 import MainTitle from '../Presentation/MainTitle';
+import { exclusiveDestines } from '../../data/exclusiveDestinyData';
+import ExclusiveComponent from './ExclusiveComponent';
 
 export default function ExclusiveDestinyContainer(props: {
      className: string;
@@ -11,20 +13,46 @@ export default function ExclusiveDestinyContainer(props: {
                sx={{
                     display: 'flex',
                     flexDirection: 'column',
-                    justifyContent: 'space-evenly',
+                    justifyContent: 'space-between',
                     alignItems: 'center',
+                    height: '100%',
+                    width: '100%',
+                    rowGap: (theme) => theme.spacing(8),
                }}
           >
-               <MainTitle string="Destinos exclusivos" />
-               <Typography variant="subtitle2">
-                    Explore as maravilhas naturais e culturais do Brasil através
-                    de acomodações únicas.
-               </Typography>
-               <Stack>
-                    <Box></Box>
-                    <Box></Box>
-                    <Box></Box>
-                    <Box></Box>
+               <Box
+                    sx={{
+                         width: '100%',
+                         display: 'flex',
+                         flexDirection: 'column',
+                         justifyContent: 'space-between',
+                         alignItems: 'center',
+                    }}
+               >
+                    <MainTitle
+                         align="center"
+                         string="Destinos exclusivos"
+                    />
+                    <Typography variant="subtitle2">
+                         Explore as maravilhas naturais e culturais do Brasil
+                         através de acomodações únicas.
+                    </Typography>
+               </Box>
+               <Stack
+                    direction="row"
+                    sx={{
+                         columnGap: (theme) => theme.spacing(8),
+                         padding: (theme) => theme.spacing(8),
+                    }}
+               >
+                    {exclusiveDestines.map((el) => (
+                         <ExclusiveComponent
+                              key={el.index}
+                              name={el.name}
+                              available={el.available}
+                              img={el.img}
+                         />
+                    ))}
                </Stack>
           </Box>
      );
