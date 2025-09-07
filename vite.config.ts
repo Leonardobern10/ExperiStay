34b61661cd46c1de8969a9561486b71fc76ba596
Leonardo Bernardo <leonardo.bernardo2658@gmail.com>
2025-09-07 13:00:22 -0300

    format

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,20 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import path from 'path';
 
 // https://vite.dev/config/
 export default defineConfig({
      plugins: [react()],
+     resolve: {
+          alias: {
+               '@': path.resolve(__dirname, 'src'),
+               '@components': path.resolve(__dirname, 'src/components'),
+               '@pages': path.resolve(__dirname, 'src/pages'),
+               '@images': path.resolve(__dirname, 'src/assets/images'),
+               '@data': path.resolve(__dirname, 'src/data'),
+               '@hooks': path.resolve(__dirname, 'src/hooks'),
+               '@theme': path.resolve(__dirname, 'src/theme'),
+               '@types': path.resolve(__dirname, 'src/types'),
+          },
+     },
 });
