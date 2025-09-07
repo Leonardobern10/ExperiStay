93bbe8ebca6c5e94ba609bc5e46f60f6397150a1
Leonardo Bernardo <leonardo.bernardo2658@gmail.com>
2025-09-07 15:28:59 -0300

    update: atualizando importações

---
+++ src/components/MainTitle.tsx
@@ -0,0 +1,33 @@
+import { Typography } from '@mui/material';
+import { useMemo, type ReactElement } from 'react';
+
+export default function MainTitle(props: {
+     string: string;
+     align?: 'center' | 'flex-end' | 'flex-start';
+}): ReactElement {
+     const strings = useMemo(() => {
+          let defaultText: Array<string> = props.string.split(' ');
+          let coloredText = defaultText.splice(-1);
+
+          return [defaultText.join(' '), coloredText[0]];
+     }, [props.string]);
+
+     return (
+          <Typography
+               color={'textPrimary'}
+               variant="h2"
+               sx={{
+                    width: '100%',
+                    textShadow: '2px 2px 2px black',
+                    textAlign: `${props.align && props.align}`,
+               }}>
+               {strings[0]}
+               <Typography
+                    variant="h2"
+                    component={'span'}
+                    color="#ab9f19ff">
+                    {` ${strings[1]}`}
+               </Typography>
+          </Typography>
+     );
+}
