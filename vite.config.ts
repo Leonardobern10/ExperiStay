ed2d95842d1bc0003770cb9d25e609a1f6238861
Leonardo Bernardo <leonardo.bernardo2658@gmail.com>
2025-09-26 20:53:03 -0300

    new: alias para a pasta services

--- vite.config.ts
+++ vite.config.ts
@@ -4,17 +4,18 @@ import path from 'path';
 
 // https://vite.dev/config/
 export default defineConfig({
-     plugins: [react()],
-     resolve: {
-          alias: {
-               '@': path.resolve(__dirname, 'src'),
-               '@components': path.resolve(__dirname, 'src/components'),
-               '@pages': path.resolve(__dirname, 'src/pages'),
-               '@images': path.resolve(__dirname, 'src/assets/images'),
-               '@data': path.resolve(__dirname, 'src/data'),
-               '@hooks': path.resolve(__dirname, 'src/hooks'),
-               '@theme': path.resolve(__dirname, 'src/theme'),
-               '@types': path.resolve(__dirname, 'src/types'),
-          },
-     },
+    plugins: [react()],
+    resolve: {
+        alias: {
+            '@': path.resolve(__dirname, 'src'),
+            '@components': path.resolve(__dirname, 'src/components'),
+            '@pages': path.resolve(__dirname, 'src/pages'),
+            '@images': path.resolve(__dirname, 'src/assets/images'),
+            '@data': path.resolve(__dirname, 'src/data'),
+            '@hooks': path.resolve(__dirname, 'src/hooks'),
+            '@theme': path.resolve(__dirname, 'src/theme'),
+            '@types': path.resolve(__dirname, 'src/types'),
+            '@services': path.resolve(__dirname, 'src/services'),
+        },
+    },
 });
