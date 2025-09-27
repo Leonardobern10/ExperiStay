c8efae91b9bba834f14c50da28704485c56b7044
Leonardo Bernardo <leonardo.bernardo2658@gmail.com>
2025-09-26 22:03:36 -0300

    new: criando tipagem

---
+++ src/components/home/UnbelivableExperiences/unbelivableExperiences.types.ts
@@ -0,0 +1,12 @@
+import type { SxProps, Theme } from '@mui/material';
+
+export type ExperienceComponentSxType = {
+    cardSx: SxProps<Theme>;
+    boxTitleSx: SxProps<Theme>;
+    boxAlertSx: SxProps<Theme>;
+};
+
+export type UnbelivableExperiencesSxType = {
+    boxTitleSx: SxProps<Theme>;
+    boxMobileSx: SxProps<Theme>;
+};
