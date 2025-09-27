e474e37c5e35b7c16a4b3dbfa59523cf44685a13
Leonardo Bernardo <leonardo.bernardo2658@gmail.com>
2025-09-26 22:19:43 -0300

    new: adicionando tipagem a seção

---
+++ src/components/Header/header.types.ts
@@ -0,0 +1,12 @@
+import type { SxProps, Theme } from '@mui/material';
+
+export type HeaderDesktopSxType = {
+    stackSx: SxProps<Theme>;
+    logoSx: SxProps<Theme>;
+    boxSx: SxProps<Theme>;
+};
+
+export type HeaderMobileSxType = {
+    stackSx: SxProps<Theme>;
+    navSx: SxProps<Theme>;
+};
