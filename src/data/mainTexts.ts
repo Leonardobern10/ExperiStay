be8260d01532df0e3785c7eeef1e5601c438eae8
Leonardo Bernardo <leonardo.bernardo2658@gmail.com>
2025-10-07 22:53:59 -0300

    remove

--- src/data/mainTexts.ts
+++ src/data/mainTexts.ts
@@ -1,14 +1,6 @@
 import type { MainTextProps } from '../types/MainTextProps';
 
 export const mainTexts: MainTextProps = {
-     title: 'Sua próxima experiência começa aqui.',
-     desc: 'Mais do que um lugar para ficar, oferecemos o cenário perfeito para as suas melhores experiências.',
+    title: 'Sua próxima experiência começa aqui.',
+    desc: 'Mais do que um lugar para ficar, oferecemos o cenário perfeito para as suas melhores experiências.',
 };
-/*
-let text1 =
-     'A ExperiStay é a sua plataforma de acesso a um universo de imóveis por temporada, situados em pontos estratégicos e vibrantes.';
-let text2 =
-     'Mais do que um lugar para ficar, oferecemos o cenário perfeito para as suas melhores experiências.';
-let text3 =
-     'De um refúgio tranquilo a um ponto de partida para explorar a cidade, cada imóvel da ExperiStay é pensado para enriquecer a sua viagem.';
-*/
